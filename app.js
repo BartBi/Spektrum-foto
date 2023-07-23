4386222c6a6f3e33ecdc44884d2d44e79830473a
Bartek Bławat <bartloomiejblawat@gmail.com>
2023-07-23 16:50:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelector(".nav-links"); 
+
+const navBtn = document.querySelector(".nav-btn"); 
+
+navBtn.addEventListener("click",() => {
+    navLinks.classList.toggle("show-links");
+  
+    
+});
+
+const date = document.getElementById("date");
+const currentYear = new Date().getFullYear();
+
+date.innerHTML = currentYear ; 
+
