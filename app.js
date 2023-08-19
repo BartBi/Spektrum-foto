91038d51e45fb811385ffba011d37cce9aaf91ea
Bartek Bławat <bartloomiejblawat@gmail.com>
2023-08-19 21:07:41 +0200

    add intesection observer on images

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const navBtn = document.querySelector(".nav-btn");
 
 navBtn.addEventListener("click", () => {
     navLinks.classList.toggle("show-links");
-
-
 });
 
 const date = document.getElementById("date");
@@ -13,3 +11,22 @@ const currentYear = new Date().getFullYear();
 
 date.innerHTML = currentYear;
 
+const images = document.querySelectorAll(".image");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting);
+
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target)
+        }
+    });
+},
+    {
+        threshold: 0.8
+    }
+);
+
+images.forEach(image => {
+    observer.observe(image);
+});
