ef57d70ff0d87993a7676b63bf0737a10b5a8cb7
Bartek Bławat <bartloomiejblawat@gmail.com>
2023-10-17 11:22:38 +0200

    add better quality images
    for large size view, add large view and then Img slider

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const currentYear = new Date().getFullYear();
 
 date.innerHTML = currentYear;
 
-const images = document.querySelectorAll(".image");
+let images = document.querySelectorAll(".image");
+images = [...images] //making array from "nodes array" 
 
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
@@ -25,26 +26,97 @@ const observer = new IntersectionObserver(entries => {
 },
     {
         threshold: 0,
-        rootMargin: '100px',
+        rootMargin: '0px',
     }
 );
 
 images.forEach(image => {
-    observer.observe(image);  
+    observer.observe(image);
 });
 
 
-let images2 = document.querySelectorAll(".image");
 
-images2 = [...images2];
+let page = document.querySelectorAll(".container");//container selected 
+console.log(page)
+let mainPage = document.querySelector(".page");
+
+// let slider = document.querySelector(".sliderImg");
+
+let img = document.querySelectorAll('img');
+
+page = [...page];   // making array from "nodes array"
 
 
-    for(let i = 0; i< images2.length ;i++){
-        images2[i].addEventListener("click",function (){
 
-            // images2[i].classList.toggle("large");
-           
-        });
+for (let i = 0; i < images.length; i++) {
+
+    let currentImg;
+    let sliderPrev;
+    let sliderNext;
+    sliderNext = document.createElement("a"); // create prev button container
+    let slider = document.createElement("div"); // create slider container
+    sliderPrev = document.createElement("a"); // create prev button container
+    let b
+
+    function sliderComponent(e) {
+        page.forEach(j => j.style.display = 'none') // disable grid display
+        
+        slider.classList.add("sliderImg"); // add classlist for slider container 
+        mainPage.append(slider); // appen slider container 
+
+        
+        sliderPrev.classList.add("prev"); // add classlist for prev button
+        sliderPrev.innerHTML = "&#10094;" // add deicamlCode for prev butoon 
+        sliderPrev.style.color = "var(--grey-400)";
+        sliderPrev.style.color
+        slider.append(sliderPrev); // append prevbutton
+        slider.append(images[i]); // append current image container
+        sliderNext.classList.add("next"); // add classlist for prev button
+        sliderNext.innerHTML = "&#10095;"
+        sliderNext.style.color = "var(--grey-400)";
+        slider.append(sliderNext);
+       
+        // change for better quality 
+        currentImg = images[i].children[0];
+        let test1 = currentImg.src.replace(/gallery\W/, "gallery2/");
+        currentImg.src = test1;
+        images[b].removeEventListener("click", sliderComponent);
+        //remove add event lister for image click
+        images[i].removeEventListener("click", sliderComponent);
+        
+        //change current image 
+        
     }
-
     
+    sliderNext.addEventListener("click", function () {
+        
+        b = i++
+        
+        
+        let currentImg = images[i].children[0];
+         test1 = currentImg.src.replace(/gallery\W/, "gallery2/");
+        currentImg.src = test1;
+        slider.replaceChild(images[i],images[b]);
+       
+
+
+    })
+
+    images[i].addEventListener("click", sliderComponent);
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
